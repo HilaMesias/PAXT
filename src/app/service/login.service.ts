52b454dce666f2647031a6a9ec2d52ec1c2a55a1
HilaMesias <93962435+HilaMesias@users.noreply.github.com>
2023-08-11 18:22:04 -0500

    11/08

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { JwtRequest } from '../model/jwtRequest';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(request: JwtRequest) {
+    return this.http.post("http://localhost:8080/authenticate", request);
+  }
+
+
+  verificar() {
+    let token = sessionStorage.getItem("token");
+    return token != null;
+
+  }
+  showRole(){
+    let token = sessionStorage.getItem("token");
+    const helper = new JwtHelperService();
+    const decodedToken = helper.decodeToken(token);
+    return decodedToken?.role;
+  }
+
 }
