a3d2e4e31ef87641e07ff07c85fa61950c9e92ff
HilaMesias <93962435+HilaMesias@users.noreply.github.com>
2023-09-14 10:00:20 -0500

    registrar

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,6 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
+
+import { FormGroup, FormControl } from '@angular/forms';
+import { TrabajadorService } from 'src/app/service/trabajador.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Trabajador } from 'src/app/model/Trabajador';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,7 +16,9 @@ export class NavbarComponent {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
 
-  constructor(private observer: BreakpointObserver) {}
+  constructor(private observer: BreakpointObserver, private aS: TrabajadorService, 
+    private router: Router, 
+    private route: ActivatedRoute) {}
 
   ngAfterViewInit() {
     this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
@@ -22,5 +30,61 @@ export class NavbarComponent {
         this.sidenav.open();
       }
     });
+  }
+  form: FormGroup = new FormGroup({});
+  author: Trabajador = new Trabajador();
+  mensaje: string = "";
+  id: number = 0;
+  edicion: boolean = false;
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init();
+    })
+    this.form = new FormGroup({
+      id: new FormControl(),
+      nombre: new FormControl(),
+      empresa: new FormControl(),
+    });
+  }
+  aceptar(): void {
+    this.author.id = this.form.value['id'];
+    this.author.nombre = this.form.value['nombre'];
+    this.author.empresa = this.form.value['empresa'];
+    if (this.form.value['nombre'].length > 0 &&
+      this.form.value['empresa'].length > 0) {
+
+      if (this.edicion) {
+        this.aS.update(this.author).subscribe((data) => {
+          this.aS.list().subscribe(data => {
+            this.aS.setList(data);
+          })
+        })
+      } else {
+        this.aS.insert(this.author).subscribe((data)=> {
+          this.aS.list().subscribe(data => {
+            this.aS.setList(data);
+          })
+        })
+      }
+      this.router.navigate(['/pages/autores']);
+    } else {
+      this.mensaje = "Complete los campos requeridos!!!";
+    }
+  }
+
+  init() {
+    if (this.edicion) {
+      this.aS.listId(this.id).subscribe(data => {
+        this.form = new FormGroup({
+          id: new FormControl(data.id),
+          nombre: new FormControl(data.nombre),
+          empresa: new FormControl(data.empresa),
+        })
+      })
+    }
   }
 }
--- src/app/component/registrarse/registrarse.component.ts
+++ src/app/component/registrarse/registrarse.component.ts
@@ -9,65 +9,65 @@ import { TrabajadorService } from 'src/app/service/trabajador.service';
   templateUrl: './registrarse.component.html',
   styleUrls: ['./registrarse.component.css']
 })
-export class RegistrarseComponent {
-  form: FormGroup = new FormGroup({});
-  author: Trabajador = new Trabajador();
-  mensaje: string = "";
-  id: number = 0;
-  edicion: boolean = false;
+ export class RegistrarseComponent {
+//   form: FormGroup = new FormGroup({});
+//   author: Trabajador = new Trabajador();
+//   mensaje: string = "";
+//   id: number = 0;
+//   edicion: boolean = false;
 
-  constructor(private aS: TrabajadorService, 
-    private router: Router, 
-    private route: ActivatedRoute) {
-  }
+//   constructor(private aS: TrabajadorService, 
+//     private router: Router, 
+//     private route: ActivatedRoute) {
+//   }
 
-  ngOnInit(): void {
-    this.route.params.subscribe((data: Params) => {
-      this.id = data['id'];
-      this.edicion = data['id'] != null;
-      this.init();
-    })
-    this.form = new FormGroup({
-      id: new FormControl(),
-      nombre: new FormControl(),
-      empresa: new FormControl(),
-    });
-  }
-  aceptar(): void {
-    this.author.id = this.form.value['id'];
-    this.author.nombre = this.form.value['nombre'];
-    this.author.empresa = this.form.value['empresa'];
-    if (this.form.value['nombre'].length > 0 &&
-      this.form.value['empresa'].length > 0) {
+//   ngOnInit(): void {
+//     this.route.params.subscribe((data: Params) => {
+//       this.id = data['id'];
+//       this.edicion = data['id'] != null;
+//       this.init();
+//     })
+//     this.form = new FormGroup({
+//       id: new FormControl(),
+//       nombre: new FormControl(),
+//       empresa: new FormControl(),
+//     });
+//   }
+//   aceptar(): void {
+//     this.author.id = this.form.value['id'];
+//     this.author.nombre = this.form.value['nombre'];
+//     this.author.empresa = this.form.value['empresa'];
+//     if (this.form.value['nombre'].length > 0 &&
+//       this.form.value['empresa'].length > 0) {
 
-      if (this.edicion) {
-        this.aS.update(this.author).subscribe((data) => {
-          this.aS.list().subscribe(data => {
-            this.aS.setList(data);
-          })
-        })
-      } else {
-        this.aS.insert(this.author).subscribe((data)=> {
-          this.aS.list().subscribe(data => {
-            this.aS.setList(data);
-          })
-        })
-      }
-      this.router.navigate(['/pages/autores']);
-    } else {
-      this.mensaje = "Complete los campos requeridos!!!";
-    }
-  }
+//       if (this.edicion) {
+//         this.aS.update(this.author).subscribe((data) => {
+//           this.aS.list().subscribe(data => {
+//             this.aS.setList(data);
+//           })
+//         })
+//       } else {
+//         this.aS.insert(this.author).subscribe((data)=> {
+//           this.aS.list().subscribe(data => {
+//             this.aS.setList(data);
+//           })
+//         })
+//       }
+//       this.router.navigate(['/pages/autores']);
+//     } else {
+//       this.mensaje = "Complete los campos requeridos!!!";
+//     }
+//   }
 
-  init() {
-    if (this.edicion) {
-      this.aS.listId(this.id).subscribe(data => {
-        this.form = new FormGroup({
-          id: new FormControl(data.id),
-          nombre: new FormControl(data.nombre),
-          empresa: new FormControl(data.empresa),
-        })
-      })
-    }
-  }
+//   init() {
+//     if (this.edicion) {
+//       this.aS.listId(this.id).subscribe(data => {
+//         this.form = new FormGroup({
+//           id: new FormControl(data.id),
+//           nombre: new FormControl(data.nombre),
+//           empresa: new FormControl(data.empresa),
+//         })
+//       })
+//     }
+//   }
 }
